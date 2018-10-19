b284e303e97f5f7607131817c9c5849adc004255
yanlijing <yan@yanlijingdeMacBook-Pro.local>
2018-10-19 10:13:09 +0800

    @babel配置

--- react-bucket/src/index.js
+++ react-bucket/src/index.js
@@ -1,18 +1,29 @@
-import _ from 'lodash';
-import Icon from './img.png';
-import './style.css';
+//import _ from 'lodash';
+//import  React from 'react';
+//import ReactDom from 'react-dom';
+//import './style/style.css';
 
-function component(){
-	var element = document.createElement('div');
-	element.innerHTML = _.join(['Hello,','webpack'], '');
-	element.classList.add('hello');
-	
-	var myIcon = new Image();
-	myIcon.src = Icon;
+//  function component() {
+//    var element = document.createElement('h1');
+//
+//    // lodash 是由当前 script 脚本 import 导入进来的
+//    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//    element.classList.add('hello');
+//
+//    return element;
+//  }
 
-	element.appendChild(myIcon);
+//class Hello extends React.Component {
+//	render(){
+//		return <h2>我是React生成的</h2>
+//	}
+//}
+//document.getElementById('root').appendChild(<Hello/>);
 
-	return element;
-}
+import React from "react";
+import ReactDOM from "react-dom";
 
-document.body.appendChild(component());
+ReactDOM.render(
+    <h1>Hello, World!</h1>,
+    document.getElementById('root')
+);
--- react-bucket/webpack.config.js
+++ react-bucket/webpack.config.js
@@ -1,26 +1,51 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle222.js',
+   entry: './src/index.js',
+   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+	devtool:'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	},
+	plugins: [
+	  new CleanWebpackPlugin(['dist']),
+	  new HtmlWebpackPlugin({
+      title:'HtmlWebpackPlugin',
+      template: './index.html'
+	  }),
+	  new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     rules: [
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader',
-          ]
-		},
-		{
-			test:/\.(png|svg|jpg|gif)$/,
-			use: [
-				'file-loader'
-			]
-		}
-      ]
-    }
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/,
+         use: [
+           'file-loader'
+         ]
+	   },
+		{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+		{ test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+	mode: 'none',
 };
